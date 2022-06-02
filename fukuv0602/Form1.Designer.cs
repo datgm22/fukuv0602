224e3e498dc184e150e70c0ba76270d132471765
A601-00 <tnk.dat.class@gmail.com>
2022-06-02 13:56:16 +0900

    おまけ

--- fukuv0602/Form1.Designer.cs
+++ fukuv0602/Form1.Designer.cs
@@ -39,11 +39,12 @@ namespace fukuv0602
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(285, 182);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(98, 45);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "(・ω・)";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button1
--- fukuv0602/Form1.cs
+++ fukuv0602/Form1.cs
@@ -20,26 +20,31 @@ namespace fukuv0602
         private void button1_Click(object sender, EventArgs e)
         {
             label1.Top = label1.Top - 10;
+            label1.Text = "(^ω^)";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             label1.Left = label1.Left - 10;
+            label1.Text = "(・ω・#)";
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             label1.Left = label1.Left + 10;
+            label1.Text = "(#・ω・)";
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             label1.Top = label1.Top + 10;
+            label1.Text = "(_ω_)";
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("田中雄");
+            label1.Visible = false;
         }
     }
 }
